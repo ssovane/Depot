a049fc675b4e87f5ae64e2a646cd71ee6f028074
melkaya <sandra_sovane@inbox.lv>
2011-06-28 10:09:18 +0300

    Depot

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,15 @@
+class StoreController < ApplicationController
+
+  def index
+    @products = Product.all
+    @count = increment_count
+  end
+
+  def increment_count
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '8ae2870b103dc2400cd226b2027d0bb1d08f55acc388dad9148c3ccd6e276dde513a9204a5257f3063b3f150f64a18527d008b2e2243ead422fe93cea44db157'
